5dd353cc30980d21cf1dbad8f34e1bad1a323d76
gabrieldiem <gabriel.diem5@gmail.com>
2025-10-05 21:56:35 -0300

    testing webook

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div className="card">
+        <p>
+          Trying out webhook
+        </p>
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
